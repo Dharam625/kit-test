5e2e479933f6abcec33c06dc605a7c5b74e973d7
amita dhiman <amitadhiman@amitas-MacBook-Air.local>
2016-08-29 00:46:41 +0530

    rename variables

--- app/scripts/controllers/pigLatinTranslator.js
+++ app/scripts/controllers/pigLatinTranslator.js
@@ -8,24 +8,20 @@
  * Controller of the kitTestApp
  */
 angular.module('kitTestApp').controller('PigLatinTranslatorCtrl', function ($scope, utilityService) {
-
     $scope.changeToPigLatin = function(enteredWord) {
        var regex = /[aeiou]/gi;
        if (enteredWord.length > 0 &&  enteredWord.match(regex) !== null) {
-
-          var vowelIndex = enteredWord.indexOf(enteredWord.match(regex)[0]);
-          var first = enteredWord.slice(0, vowelIndex);
-          var sLast = enteredWord.slice(vowelIndex);
-
-            if (utilityService.isVowel(enteredWord[0])) {
-              $scope.output = enteredWord + 'way';
-            }
-            else {
-              $scope.output = sLast + first + 'ay';
-            }
+          if (utilityService.isVowel(enteredWord[0])) {
+            $scope.output = enteredWord + 'way';
+          }
+          else {
+            var vowelIndex = enteredWord.indexOf(enteredWord.match(regex)[0]);
+            var consonantsPreVowel = enteredWord.slice(0, vowelIndex); // extract consonants cluster before vowel
+            var vowel = enteredWord.slice(vowelIndex);
+            $scope.output = vowel + consonantsPreVowel + 'ay';
+          }
         } else { // Else set the output to same as input
           $scope.output = enteredWord;
         }
     };
-
   });
